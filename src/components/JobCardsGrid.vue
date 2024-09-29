8d0983898a8066af1baebb07f4ce22faec9945d1
elakapmain <notquadrilingual@gmail.com>
2024-09-29 14:34:55 -0500

    Correctly install Tailwind for proper hot reloading during development

--- src/components/JobCardsGrid.vue
+++ src/components/JobCardsGrid.vue
@@ -3,7 +3,7 @@ import JobCardSingle from './JobCardSingle.vue';
 </script>
 
 <template>
-    <div class="grid grid-cols-4 gap-10 p-10">
+    <div class="grid grid-cols-3 gap-10 p-10">
         <JobCardSingle v-for="card in 20" :key="card"></JobCardSingle>
     </div>
 </template>
