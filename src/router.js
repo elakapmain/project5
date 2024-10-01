19a763f6d2431f4f0860835a8cd8345261760f16
elakapmain <notquadrilingual@gmail.com>
2024-10-01 15:31:57 -0500

    Add navigation guard for profile page

--- src/router.js
+++ src/router.js
@@ -5,17 +5,29 @@ import JobDetails from './views/JobDetails.vue'
 import LoginPage from './views/LoginPage.vue'
 import ProfilePage from './views/ProfilePage.vue'
 
+import { useAuth } from './composables/useAuth'
+const { isAuthenticated } = useAuth()
+
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
   { path: '/job/:id', name: 'JobDetails', component: JobDetails },
   { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/profile', name: 'ProfilePage', component: ProfilePage },
+  { path: '/profile', name: 'ProfilePage', component: ProfilePage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+//  Navigation guard (for settings page)
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({name: 'LoginPage', query: {redirect: to.fullPath}})
+  } else {
+    next()
+  }
+}) //  Will run before actually routing pages
 
 export default router
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,3 +1,5 @@
 <template>
-    Profile Page
+    <main class="flex min-h-screen items-center justify-center">
+    <h1 class="text-6xl font-thin text-slate-800">Your Profile Page</h1>
+  </main>
 </template>
